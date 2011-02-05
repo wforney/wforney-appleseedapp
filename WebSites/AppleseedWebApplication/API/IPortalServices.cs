28b3babc1254310f617bbe0106d95888f4aa0606
ramseur <ramseur@gmail.com>
2011-02-04 21:04:09 -0500

    added methods for service

--- WebSites/AppleseedWebApplication/API/IPortalServices.cs
+++ WebSites/AppleseedWebApplication/API/IPortalServices.cs
@@ -11,7 +11,64 @@ namespace AppleseedWebApplication.API
     [ServiceContract]
     public interface IPortalServices
     {
+
+
+        #region Users
+
         [OperationContract]
-        void DoWork();
+        void GetUsers();
+        [OperationContract]
+        void GetRoles();
+        [OperationContract]
+        void CreateUser();
+        [OperationContract]
+        void CreateRole();
+        [OperationContract]
+        void AddUserToRole();
+        [OperationContract]
+        void RemoveUserFromRole();
+        [OperationContract]
+        void AddUserToPortal();
+        [OperationContract]
+        void RemoveUserFromPortal();
+        #endregion
+
+        #region Pages
+
+        [OperationContract]
+        void GetPages();
+        [OperationContract]
+        void CreatePageInPortal();
+        #endregion
+
+        #region Modules
+
+        [OperationContract]
+        void GetModulesOnPage();
+        [OperationContract]
+        void GetAllInstalledModules();
+        [OperationContract]
+        void UnInstallModule();
+        [OperationContract]
+        void AddModuleToPage();
+        [OperationContract]
+        void UpdateModuleInfo();
+        [OperationContract]
+        void EditModuleData();
+        //- For Html/Content modules only
+        //InstallModule- future
+        #endregion
+
+        #region Portal
+
+        [OperationContract]
+        void GetPortals();
+        [OperationContract]
+        void CreatePortal();
+        [OperationContract]
+        void ChangePortalSettings();
+        [OperationContract]
+        void RemovePortal();
+        #endregion
     }
 }
