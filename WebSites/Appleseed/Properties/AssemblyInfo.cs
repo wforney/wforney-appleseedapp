39738ceb88f0776cb8cafc9af8c62fe052af8f03
Miguel Gimenez <miguel.gimenez@agileworks.com.uy>
2011-02-11 00:27:19 -0200

    Rename AppleseedWebApplication to Appleseed

--- WebSites/Appleseed/Properties/AssemblyInfo.cs
+++ WebSites/Appleseed/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AppleseedWebApplication")]
+[assembly: AssemblyTitle("Appleseed")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("AppleseedWebApplication")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("Appleseed")]
+[assembly: AssemblyProduct("Appleseed")]
+[assembly: AssemblyCopyright("Copyright © Appleseed 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
